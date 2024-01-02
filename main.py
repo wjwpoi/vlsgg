0fb95eed75e3c750687e6a5ee255edebfae7903a
wjw <wjwpoi@buaa.edu.cn>
2024-01-02 20:44:53 +0800

    new evaluation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from transformers import AutoImageProcessor, AutoTokenizer
 from utils.dataloader import load_data, load_vg_dict
-from utils.model import SGGModel, SetCriterion
+from utils.model import SGGModel, SetCriterion, PostProcess
 from torch.utils.data import DataLoader
 from utils.misc import collate_fn
 from utils.matcher import HungarianMatcher
@@ -11,6 +11,7 @@ import os
 import torch
 import torch.nn as nn
 from dataset import build_dataset, get_coco_api_from_dataset
+from utils.evaluation import evaluate
 
 
 def set_seed(seed_value):
@@ -31,13 +32,14 @@ dataset_name = 'vg'
 num_classes = 151 if dataset_name != 'oi' else 289 # some entity categories in OIV6 are deactivated.
 num_rel_classes = 51 if dataset_name != 'oi' else 31
 weight_dict = {'loss_ce': 1, 'loss_bbox': 5, 'loss_giou': 2, 'loss_rel': 1}
+postprocessors = {'bbox': PostProcess()}
 
 
 set_seed(621)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-image_model = "/home/wjw/checkpoints/weight/deformable-detr"
-text_model = "/home/wjw/checkpoints/weight/roberta-base"
+image_model = "/home/wjw/checkpoints/weight/deformable-detr/"
+text_model = "/home/wjw/checkpoints/weight/roberta-base/"
 
 idx_to_label, idx_to_predicate, label_list, predicate_list = load_vg_dict('/home/wjw/data/VG/VG-SGG-dicts-with-attri.json')
 processor = AutoImageProcessor.from_pretrained(image_model)
@@ -53,6 +55,7 @@ tokenized_text = {k: torch.tensor(v).to(device) for k, v in tokenized_text.items
 
 dataset_train = build_dataset('train', dataset_name, '/home/wjw/data/VG/', '/home/wjw/data/VG/VG_100K/')
 dataset_val = build_dataset('val', dataset_name, '/home/wjw/data/VG/', '/home/wjw/data/VG/VG_100K/')
+base_ds = get_coco_api_from_dataset(dataset_val)
 
 train_dataloader = DataLoader(dataset_train, batch_size=2, shuffle=True, collate_fn=collate_fn)
 val_dataloader = DataLoader(dataset_val, batch_size=1, collate_fn=collate_fn)
@@ -67,41 +70,44 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-4)
 total_epoch = 20
 
 for epoch in range(total_epoch):
-    model.train()
-    with tqdm(total=len(train_dataloader)) as _tqdm:
-        _tqdm.set_description('epoch: {}/{}'.format(epoch + 1, total_epoch))
-        mean_loss = 0
-        for num, batch in enumerate(train_dataloader):
-            inputs = {'nested_pixel_values': batch[0].to(device), 
-                      'input_ids': tokenized_text['input_ids'], 'attention_mask': tokenized_text['attention_mask']}
+    # model.train()
+    # with tqdm(total=len(train_dataloader)) as _tqdm:
+    #     _tqdm.set_description('epoch: {}/{}'.format(epoch + 1, total_epoch))
+    #     mean_loss = 0
+    #     for num, batch in enumerate(train_dataloader):
+    #         inputs = {'nested_pixel_values': batch[0].to(device), 
+    #                   'input_ids': tokenized_text['input_ids'], 'attention_mask': tokenized_text['attention_mask']}
             
-            optimizer.zero_grad()
-            outputs = model(**inputs)
-            targets = [{k: v.to(device) for k, v in t.items()} for t in batch[1]]
-            loss_dict = criterion(outputs, targets)
-            loss = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
+    #         optimizer.zero_grad()
+    #         outputs = model(**inputs)
+    #         targets = [{k: v.to(device) for k, v in t.items()} for t in batch[1]]
+    #         loss_dict = criterion(outputs, targets)
+    #         loss = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
 
-            loss.backward()
-            optimizer.step()
+    #         loss.backward()
+    #         optimizer.step()
 
-            mean_loss += loss.item()
-            _tqdm.set_postfix(loss='{:.4f}'.format(loss.item()), mean_loss='{:.4f}'.format(mean_loss/(num+1)))
-            _tqdm.update(1)
+    #         mean_loss += loss.item()
+    #         _tqdm.set_postfix(loss='{:.4f}'.format(loss.item()), mean_loss='{:.4f}'.format(mean_loss/(num+1)))
+    #         _tqdm.update(1)
     
-    torch.save(model, "/home/wjw/checkpoints/model.pt")
+    # torch.save(model, "/home/wjw/checkpoints/model.pt")
         
     if (epoch + 1) % 5 == 0 or epoch == 0 or 1:
-        model.eval()
-        criterion.eval()
-        recall_all = [0] * 3
+        # model.eval()
+        # criterion.eval()
+        # recall_all = [0] * 3
         print('########## START TEST ##########')
-        for batch in tqdm(val_dataloader):
-            inputs = {'nested_pixel_values': batch[0].to(device), 
-                    'input_ids': tokenized_text['input_ids'], 'attention_mask': tokenized_text['attention_mask']}
-            outputs = model(**inputs)
-            targets = [{k: v.to(device) for k, v in t.items()} for t in batch[1]]
-            recall_all = model.evaluate(outputs, targets, matcher, recall_all)
+        # for batch in tqdm(val_dataloader):
+        #     inputs = {'nested_pixel_values': batch[0].to(device), 
+        #             'input_ids': tokenized_text['input_ids'], 'attention_mask': tokenized_text['attention_mask']}
+        #     outputs = model(**inputs)
+        #     targets = [{k: v.to(device) for k, v in t.items()} for t in batch[1]]
+        #     recall_all = model.evaluate(outputs, targets, matcher, recall_all)
 
-        recall_all = [float(recall/len(dataset_val)) for recall in recall_all]
-        print(recall_all)
+        # recall_all = [float(recall/len(dataset_val)) for recall in recall_all]
+        # print(recall_all)
+        test_stats, coco_evaluator = evaluate(model, criterion, postprocessors, val_dataloader, base_ds, device, dataset_name, tokenized_text)
+
+        print(test_stats)
 
